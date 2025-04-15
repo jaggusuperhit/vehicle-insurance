cceee9b805ef09989899a75fd0138340ac60861a
jaggusuperhit <jagtapsuraj636@gmail.com>
2025-04-15 17:09:44 +0530

    project srtucture added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Suraj Jagtap",
+    author_email="jagtapsuraj636@gmail.com",
+    packages=find_packages(),
+)
