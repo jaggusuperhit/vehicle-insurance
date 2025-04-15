24a4b4b5f9d8df2cfd8f610ae18cb69466745d2f
jaggusuperhit <jagtapsuraj636@gmail.com>
2025-04-16 02:54:33 +0530

    data validation

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -7,11 +7,12 @@ class DataIngestionArtifact:
     test_file_path: str
 
 
-# @dataclass
-# class DataValidationArtifact:
-#     validation_status:bool
-#     message: str
-#     validation_report_file_path: str
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    message: str
+    validation_report_file_path: str
+
 
 # @dataclass
 # class DataTransformationArtifact:
